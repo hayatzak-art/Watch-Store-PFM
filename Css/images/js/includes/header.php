942e92a02d431259461263601fb633675e8b69af
Zak Hayat <hayat_zak@um5ac.ma>
2026-06-01 16:14:58 +0100

    feat(frontend): add homepage products cart login register

---
+++ Css/images/js/includes/header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>EPOCHE Store</title>
+<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+<nav class="navbar">
+    <div class="logo">EPOCHE</div>
+
+    <ul>
+        <li><a href="index.php">Accueil</a></li>
+        <li><a href="products.php">Montres</a></li>
+        <li><a href="#">Marques</a></li>
+        <li><a href="#">Nouveautés</a></li>
+        <li><a href="cart.php">Panier</a></li>
+    </ul>
+</nav>
